6bf4ce24f635e0772e4985e382fcbcdc62a48f42
anj0la <anjola.aina@gmail.com>
2025-03-20 23:40:57 -0600

    Did a HUGE refactoring of the Vector2 class from old Penguin2D. Base Vector class is now a float instead of a template class for simplicity, since SDL supports both float and ints.

---
+++ include/penguin_framework/math/vector2.hpp
@@ -0,0 +1,93 @@
+#pragma once
+
+#include <cmath>
+
+struct [[nodiscard]] Vector2 {
+	static constexpr float ORIGIN = 0.0; /// The origin for all Vector2 objects -> (0, 0)
+
+    float x, y; /// The x and y components of the vector.
+
+	// Default constructor (x, y = ORIGIN)
+	constexpr Vector2() : x{ ORIGIN }, y{ ORIGIN } {};
+
+	// Parameterized constructor
+	constexpr Vector2(float p_x, float p_y) : x{ p_x }, y{ p_y } {}
+
+	// Copy constructor
+	constexpr Vector2(const Vector2& v) = default;
+
+	// Move constructor
+	constexpr Vector2(Vector2&& v) noexcept = default;
+
+	// Copy assignment operator
+	constexpr Vector2& operator=(const Vector2& v) = default;
+
+	// Move assignment operator
+	constexpr Vector2& operator=(Vector2&& v) noexcept = default;
+
+    // Binary operators
+
+	friend constexpr Vector2 operator+(const Vector2& v) const { return Vector2(x + v.x, y + v.y); }
+    friend constexpr Vector2 operator-(const Vector2& v) const { return Vector2(x - v.x, y - v.y); }
+    friend constexpr Vector2 operator*(const Vector2& v) const { return Vector2(x * v.x, y * v.y); }
+    friend constexpr Vector2 operator/(const Vector2& v) const { return Vector2(x / v.x, y / v.y); }
+
+	// Assignment operators
+   
+	constexpr Vector2& operator+=(const Vector2& v) const { x += v.x; y += v.y; return *this; }
+    constexpr Vector2& operator-=(const Vector2& v) const { x -= v.x; y -= v.y; return *this; }
+    constexpr Vector2& operator*=(const Vector2& v) const { x *= v.x; y *= v.y; return *this; }
+    constexpr Vector2& operator/=(const Vector2& v) const { x /= v.x; y /= v.y; return *this; }
+
+	// Comparison operators
+
+	friend constexpr bool operator==(const Vector2& v) const { return x == v.x && y == v.y; }
+	friend constexpr bool operator!=(const Vector2& v) const { return x != v.x || y != v.y; }
+
+	// Unary operators
+
+	constexpr Vector2 operator-() const { return Vector2(-x, -y); }
+	// Check if the vector is the origin.
+	constexpr bool operator!() const { return x == ORIGIN && y == ORIGIN; }
+
+	// Scalar operators
+
+	friend constexpr Vector2 operator+(float scalar) const { return Vector2(x + scalar, y + scalar); }
+	friend constexpr Vector2 operator-(float scalar) const { return Vector2(x - scalar, y - scalar); }
+	friend constexpr Vector2 operator*(float scalar) const { return Vector2(x * scalar, y * scalar); }
+	friend constexpr Vector2 operator/(float scalar) const { return Vector2(x / scalar, y / scalar); }
+
+	// Scalar assignment operators
+
+	constexpr Vector2& operator+=(float scalar) const { x += scalar; y += scalar; return *this; }
+	constexpr Vector2& operator-=(float scalar) const { x -= scalar; y -= scalar; return *this; }
+	constexpr Vector2& operator*=(float scalar) const { x *= scalar; y *= scalar; return *this; }
+	constexpr Vector2& operator/=(float scalar) const { x /= scalar; y /= scalar; return *this; }
+
+	// Scalar comparison operators
+
+	friend constexpr bool operator==(float scalar) const { return x == scalar && y == scalar; }
+	friend constexpr bool operator!=(float scalar) const { return x != scalar || y != scalar; }
+
+	// Other comparsion operators
+	
+=	constexpr auto operator<=>(const Vector2 &) const noexcept = default; // C++20 three-way comparison generates ==, !=, <, >, <=, and >= automatically
+
+	// Vector operations
+
+	Vector2 abs() const;
+	Vector2 floor() const;
+	Vector2 ceil() const;
+	Vector2 round() const;
+	Vector2 min(const Vector2& v) const;
+	Vector2 max(const Vector2& v) const;
+	Vector2 lerp(const Vector2& v, float weight) const;
+	float dot(const Vector2& v) const;
+	float cross(const Vector2& v) const;
+	float length() const;
+	float length_squared() const;
+	void normalize();
+	Vector2 normalized() const;
+	float distance_to(const Vector2& v) const;
+	float distance_squared_to(const Vector2& v) const;
+};
---
+++ src/math/vector2.cpp
@@ -0,0 +1,71 @@
+#include "include/penguin_framework/math/vector2.hpp"
+
+Vector2::abs() const {
+	return Vector2(std::fabsf(x), std::fabsf(y));
+}
+
+Vector2::floor() const {
+	return Vector2(std::floorf(x), std::floorf(y));
+}
+Vector2::ceil() const {
+	return Vector2(std::ceilf(x), std::ceilf(y));
+}
+Vector2::round() const {
+	return Vector2(std::roundf(x), std::roundf(y));
+}
+
+Vector2::min(const Vector2& v) const {
+	return Vector2(std::min(x, v.x), std::min(y, v.y));
+}
+
+Vector2::max(const Vector2& v) const {
+	return Vector2(std::max(x, v.x), std::max(y, v.y));
+}
+
+Vector2::lerp(const Vector2& v, float weight) const {
+	return Vector2(x + weight * (v.x - x), y + weight * (v.y - y));
+}
+
+float Vector2::dot(const Vector2& v) const {
+	return x * v.x + y * v.y;
+}
+
+float Vector2::cross(const Vector2& v) const {
+	return x * v.y - y * v.x;
+}
+
+float Vector2::length() const {
+	return std::sqrtf(x * x + y * y);
+}
+
+float Vector2::length_squared() const {
+	return x * x + y * y;
+}
+
+void Vector2::normalize() {
+	float len = length_squared();
+	if (len == 0) {
+		return;
+	}
+	len = std::sqrtf(len);
+	x /= len;
+	y /= len;
+	return len;
+}
+
+Vector2 Vector2::normalized() const {
+	float len = length_squared();
+	if (len == 0) {
+		return Vector2();
+	}
+	len = std::sqrtf(len);
+	return Vector2(x / len, y / len);
+}
+
+float Vector2::distance_to(const Vector2& v) const {
+	return std::sqrtf((x - v.x) * (x - v.x) + (y - v.y) * (y - v.y));
+}
+
+float Vector2::distance_squared_to(const Vector2& v) const {
+	return (x - v.x) * (x - v.x) + (y - v.y) * (y - v.y);
+}
