6105c539203cdb2d4863ddab4b4216494fdd95ed
anj0la <anjola.aina@gmail.com>
2025-04-13 22:33:17 -0600

    Added keyboard singleton with three states (current to handle repeat key presses, pressed if a key has just been pressed and released if a key has just been released).

--- include/penguin_framework/core/input/keyboard/keyboard.hpp
+++ include/penguin_framework/core/input/keyboard/keyboard.hpp
@@ -1,17 +1,29 @@
 #pragma once
 
 #include "core/input/keyboard/keyboard_keys.hpp"
-
+#include <SDL3/SDL_events.h>
 #include <array>
 
 namespace pf::core::input::keyboard {
+	struct KeyboardState {
+
+	};
+
+
 	class Keyboard {
 	public:
-		static void is_key_pressed(PF_Key key);
-		static void is_any_key_pressed();
+		static void handle_keyboard_event(const SDL_Event& p_event);
+		static bool is_key_pressed(PF_Key key);
+		static bool is_key_just_pressed(PF_Key key);
+		static bool is_key_just_released(PF_Key key);
+		static bool is_any_key_pressed();
+		static bool is_any_key_just_pressed();
+		static bool is_any_key_just_released();
+		static void update();
 
 	private:
-		std::array<bool, NUM_KEYS> key_pressed{}; // no keys are pressed initally
-
+		std::array<bool, NumKeys> current{}; // current state of the key (use to check persistence pressing)
+		std::array<bool, NumKeys> pressed{}; // keys that have just been pressed (resets every update rate)
+		std::array<bool, NumKeys> released{}; // keys that have just been released
 	};
 }
---
+++ src/core/input/keyboard/keyboard.cpp
@@ -0,0 +1,68 @@
+#include "core/input/keyboard/keyboard.hpp"
+
+using pf::core::input::keyboard::Keyboard;
+using pf::core::input::keyboard::PF_Key;
+using pf::core::input::keyboard::key_to_index;
+using pf::core::input::keyboard::InvalidKeyIndex;
+using pf::core::input::keyboard::NumKeys;
+
+void Keyboard::handle_keyboard_event(const SDL_Event& p_event) {
+	PF_Key key = static_cast<PF_Key>(p_event.key.key);
+
+	size_t i = key_to_index(key);
+	if (i == InvalidKeyIndex) return;
+
+	if (p_event.type == SDL_EVENT_KEY_DOWN) {
+
+		if (!p_event.key.repeat) pressed[i] = true;
+		current[i] = true;
+	}
+	else if (p_event.type == SDL_EVENT_KEY_UP) {
+		released[i] = true;
+		current[i] = false;
+	}
+}
+
+bool Keyboard::is_key_pressed(PF_Key key) {
+	size_t i = key_to_index(key);
+	return i != InvalidKeyIndex && current[i];
+}
+
+bool Keyboard::is_key_just_pressed(PF_Key key) {
+	size_t i = key_to_index(key);
+	return i != InvalidKeyIndex && pressed[i];
+}
+
+bool Keyboard::is_key_just_released(PF_Key key) {
+	size_t i = key_to_index(key);
+	return i != InvalidKeyIndex && released[i];
+}
+
+bool Keyboard::is_any_key_pressed() {
+	for (size_t i = 0; i < NumKeys; ++i) {
+		if (current[i]) return true;
+	}
+
+	return InvalidKeyIndex;
+}
+
+bool Keyboard::is_any_key_just_pressed() {
+	for (size_t i = 0; i < NumKeys; ++i) {
+		if (pressed[i]) return true;
+	}
+
+	return InvalidKeyIndex;
+}
+
+bool Keyboard::is_any_key_just_released() {
+	for (size_t i = 0; i < NumKeys; ++i) {
+		if (releaed[i]) return true;
+	}
+
+	return InvalidKeyIndex;
+}
+
+void Keyboard::update() {
+	pressed.fill(false);
+	released.fill(false);
+}
